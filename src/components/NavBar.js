d80b03476126b18ae22398b51fbcba84b040c0b6
Joses Syanwa <josesyanwa@gmail.com>
2023-11-08 13:27:01 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link, index) => (
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+      {index < links.length - 1 && " "} {/* Add a space if not the last link */}
+    </nav>
+  );
 }
 
 export default NavBar;
